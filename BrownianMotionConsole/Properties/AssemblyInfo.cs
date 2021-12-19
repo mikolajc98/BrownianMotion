af4239ac58ef9f8c070949182ba7e276c167be0b
Mikołaj <mikolajc98@gmail.com>
2021-12-19 10:48:16 +0100

    [Lang] - changed from Polish folder names to English

--- BrownianMotionConsole/Properties/AssemblyInfo.cs
+++ BrownianMotionConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RuchyBrownaModel")]
+[assembly: AssemblyTitle("BrownianMotionConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RuchyBrownaModel")]
+[assembly: AssemblyProduct("BrownianMotionConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b47013e7-fd78-47cb-9a89-4c52ff398fb5")]
+[assembly: Guid("525769b0-378a-42a1-b658-5cdd56a1a6c7")]
 
 // Version information for an assembly consists of the following four values:
 //
--- BrownianMotionModel/Properties/AssemblyInfo.cs
+++ BrownianMotionModel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RuchyBrownaConsole")]
+[assembly: AssemblyTitle("BrownianMotionModel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RuchyBrownaConsole")]
+[assembly: AssemblyProduct("BrownianMotionModel")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("525769b0-378a-42a1-b658-5cdd56a1a6c7")]
+[assembly: Guid("b47013e7-fd78-47cb-9a89-4c52ff398fb5")]
 
 // Version information for an assembly consists of the following four values:
 //
